df436982708ab6e141bb5f12b30df5fea509cd64
KLONNEX <battclecat154@gmail.com>
2022-09-24 16:08:44 +0600

    Update bear aug

--- src/augmentations.py
+++ src/augmentations.py
@@ -6,25 +6,35 @@ import numpy as np
 
 
 class BearAug:
-    def __init__(self, background_folder, bears_root_folder):
+    def __init__(self, background_folder, bears_root_folder, h=512, w=512):
         self.backgrounds = Path(background_folder)
         self.bears = Path(bears_root_folder)
-        self.background_names = [file.name for file in self.backgrounds.iterdir() if file.name.endswith('.JPG')]
+        self.background_names = [file.name for file in self.backgrounds.iterdir() if
+                                 file.name.endswith('.JPG') or file.name.endswith('.png')]
         self.foreground_names = [file.name for file in (self.bears / 'bears').iterdir() if file.name.endswith('.JPG')]
 
         print(f'Total bg images {len(self.background_names)}')
         print(f'Total fg images {len(self.foreground_names)}')
 
-    def gen_image(self, h, w):
+        self.h = h
+        self.w = w
+
+        self.bear_aug = A.Compose(
+            [
+                A.VerticalFlip(p=0.5),
+                A.HorizontalFlip(p=0.5),
+                A.RandomScale(scale_limit=0.2, always_apply=True, interpolation=cv2.INTER_NEAREST),
+            ]
+        )
+
+        self.random_crop = A.RandomResizedCrop(h, w)
+
+    def gen_image(self):
         """
         Add the bear from foreground to the resized background crop.
 
-        Args:
-            h (int): Output crop height.
-            w (int): Output crop width.
-
         Returns:
-            List of the generated crops.
+            List of the generated crops and list of bboxes.
         """
         bg_name = np.random.choice(self.background_names)
         fg_name = np.random.choice(self.foreground_names)
@@ -33,8 +43,9 @@ class BearAug:
         fg_mask = cv2.imread(str(self.bears / 'masks' / (Path(fg_name).stem + '.png')), cv2.IMREAD_GRAYSCALE)
 
         bears_contuors, _ = cv2.findContours(fg_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        print(f'Total bears in image {len(bears_contuors)}')
+        # print(f'Total bears in image {len(bears_contuors)}')
         bears = []
+        bboxes = []
         for bear_contour in bears_contuors:
             h_max = bear_contour[:, 0, 1].max()
             h_min = bear_contour[:, 0, 1].min()
@@ -43,16 +54,33 @@ class BearAug:
             bear_contour = np.abs(bear_contour - np.array([[w_max, h_max]]))
             bear_blank = np.zeros((h_max - h_min, w_max - w_min))
             bear_mask = cv2.drawContours(bear_blank, [bear_contour], -1, 255, -1).astype(np.bool_)
+
             bear = fg_image[h_min:h_max, w_min:w_max] * bear_mask[:, :, None]
-            bear = A.Rotate(limit=360)(image=bear)['image']
+            bear = self.bear_aug(image=bear)['image']
 
             bh, bw = bear.shape[:2]
+            rh, rw = np.random.randint(bh * 2, self.h - bh * 2), np.random.randint(bw * 2, self.w - bw * 2)
 
-            rh, rw = np.random.randint(bh * 2, h - bh * 2), np.random.randint(bw * 2, w - bw * 2)
-            blank_image = np.zeros((h, w, 3))
+            blank_image = np.zeros((self.h, self.w, 3))
             blank_image[rh:rh + bh, rw:rw + bw, :] = bear
-            random_bg = A.RandomResizedCrop(h, w)(image=bg_image)['image']
+            random_bg = self.random_crop(image=bg_image)['image']
             merged_bear = np.clip(blank_image + ((blank_image == 0) * random_bg), 0, 255).astype(np.uint8)
             bears.append(merged_bear)
 
-        return bears
+            bbox = [rw, rh + bh, rw + bw, rh]
+            bboxes.append(bbox)
+
+        return bears, bboxes
+
+    def generate_sample(self, num_images):
+        all_bears = []
+        all_bboxes = []
+        for i in range(num_images):
+            bears, bboxes = self.gen_image()
+            all_bears += bears
+            all_bboxes += bboxes
+
+            if len(all_bboxes) >= num_images:
+                break
+
+        return all_bears[:num_images], all_bboxes[:num_images]
