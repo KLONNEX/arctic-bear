154d3c3f75cd1af4e4f4f7cff59b9b881645c08c
KLONNEX <battclecat154@gmail.com>
2022-09-25 13:49:31 +0600

    Update server

--- README.md
+++ README.md
@@ -2,10 +2,31 @@
 
 ## Запуск докера
 
+Для создания докер-образа следует выполнить команду из корневой папки проекта:
+
+```bash
+docker build -t IMAGE_NAME .
+```
+
+- IMAGE_NAME - название докер образа (любое название, чтобы задать имя).
+
+Для запуска докер контейнера следует выполнить команду:
+
+```bash
+# Для пользователей Linux
+bash run_docker.sh IMAGE_NAME CONTAINER_NAME
+
+# Для пользователей Windows
+docker run -it --name CONTAINER_NAME -p 7777:7777 --runtime=nvidia --privileged=true IMAGE_NAME
+```
+
+- IMAGE_NAME - название докер образа, которое вы задали на предыдущем шаге.
+- CONTAINER_NAME - название докер контейнера (любое название, чтобы задать имя).
+
 ## Запуск сервера
 
 ```bash
-    bash run_server.py
+bash run_server.py
 ```
 
 Сервер запустится на порте 7777, подключение к нему через localhost:7777
--- run_docker.sh
+++ run_docker.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 
-if [[ $# -ne 4 ]]; then
-    echo "Usage: bash run_docker.sh [DATA_ROOT] [OUTPUT_ROOT] [IMAGE_NAME] [CONTAINER_NAME]"
+if [[ $# -ne 2 ]]; then
+    echo "Usage: bash run_docker.sh [IMAGE_NAME] [CONTAINER_NAME]"
     exit 1;
 fi
 
-get_real_path(){
-    if [ "${1:0:1}" == "/" ]; then
-        echo "$1"
-    else
-        realpath -m "$PWD/$1"
-    fi
-}
-
-DATA_ROOT=$(get_real_path "$1")
-OUTPUT_ROOT=$(get_real_path "$2")
-IMAGE_NAME="$3"
-CONTAINER_NAME="$4"
-
-if [ !  -d "$DATA_ROOT" ]; then
-  echo "Wrong path to the input dataset"
-  exit 1;
-fi
-
-if [ !  -d "$OUTPUT_ROOT" ]; then
-  mkdir "$OUTPUT_ROOT"
-fi
 
 docker run -it \
   --name "$CONTAINER_NAME" \
   -p 7777:7777 \
-  -v "$DATA_ROOT:/server/dataset" \
-  -v "$OUTPUT_ROOT:/server/outputs" \
   --runtime=nvidia \
   --privileged=true \
   "$IMAGE_NAME" \
