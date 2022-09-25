86c506bd1f9d764a6befbb9fb420860ba5494e30
goncharenko <agoncharenko1992@gmail.com>
2022-09-25 13:04:56 +0700

    first version

--- server.py
+++ server.py
@@ -1,6 +1,23 @@
 import csv
 from pathlib import Path
 
+import argparse
+import time
+from pathlib import Path
+
+import cv2
+import torch
+import torch.backends.cudnn as cudnn
+from numpy import random
+
+from models.experimental import attempt_load
+from utils.datasets import LoadStreams, LoadImages
+from utils.general import check_img_size, check_requirements, check_imshow, non_max_suppression, apply_classifier, \
+    scale_coords, xyxy2xywh, strip_optimizer, set_logging, increment_path
+from utils.plots import plot_one_box
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
+
+
 import cv2
 import flask
 import numpy as np
@@ -12,17 +29,87 @@ app.config.from_mapping(
         SECRET_KEY='dev_bears',
     )
 
-class Predict:
-    def predict(self, image):
-        return image.shape[:2]
 
-predictor = Predict()
+class Predict:
+    def __init__(self, weights, imgsz=640, device='0', conf_thres=0.25, iou_thres=0.45):
+        self.conf_thres = conf_thres
+        self.iou_thres = iou_thres
+
+        # Initialize
+        set_logging()
+        self.device = select_device(device)
+        half = self.device.type != 'cpu'  # half precision only supported on CUDA
+
+        # Load model
+        self.model = attempt_load(weights, map_location=device)  # load FP32 model
+        stride = int(self.model.stride.max())  # model stride
+        self.stride = stride
+        self.imgsz  = check_img_size(imgsz, s=stride)  # check img_size
+
+        if half:
+            self.model.half()  # to FP16
+
+    def predict(self, path_to_image_or_dir, save_txt=True, save_img=False, ):
+        # Directories
+        # Get names and colors
+        self.names = self.model.module.names if hasattr(self.model, 'module') else self.model.names
+        self.colors = [[random.randint(0, 255) for _ in range(3)] for _ in self.names]
+
+        # Set Dataloader
+        vid_path, vid_writer = None, None
+        dataset = LoadImages(path_to_image_or_dir, img_size=self.imgsz, stride=self.stride)
+
+        # Run inference
+        if self.device.type != 'cpu':
+            self.model(torch.zeros(1, 3, self.imgsz, self.imgsz).to(self.device).type_as(next(self.model.parameters())))  # run once
+
+        t0 = time.time()
+        half = self.device.type != 'cpu'  # half precision only supported on CUDA
+        for path, img, im0s, vid_cap in dataset:
+            img = torch.from_numpy(img).to(self.device)
+            img = img.half() if half else img.float()  # uint8 to fp16/32
+            img /= 255.0  # 0 - 255 to 0.0 - 1.0
+            if img.ndimension() == 3:
+                img = img.unsqueeze(0)
+
+            pred = self.model(img, augment=False)[0]
+
+            # Apply NMS
+            pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=None, agnostic=None)
+
+            # Process detections
+            for i, det in enumerate(pred):  # detections per image
+                p, s, im0, frame = path, '', im0s, getattr(dataset, 'frame', 0)
+
+                p = Path(p)  # to Path
+                save_path = str(self.save_dir / p.name)  # img.jpg
+                txt_path = str(self.save_dir / 'labels' / p.stem) + (
+                    '' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+                gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+                if len(det):
+                    # Rescale boxes from img_size to im0 size
+                    det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                    # Print results
+                    for c in det[:, -1].unique():
+                        n = (det[:, -1] == c).sum()  # detections per class
+                        s += f"{n} {self.names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                    # Write results
+                    results = []
+                    for *xyxy, conf, cls in reversed(det):
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4))).view(-1).tolist()
+                        results.append(xywh[:2])
+
+
+predictor = Predict(weights='yolo_weights/best.pt')
 
 state = {
     'filename': '',
 }
 
 header = ['image_name', 'x_center', 'y_center']
+
 
 def convert_image(bin_file):
     n_array = np.frombuffer(bin_file, dtype="uint8")
